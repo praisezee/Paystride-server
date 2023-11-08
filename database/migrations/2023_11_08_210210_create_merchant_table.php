96605c04707590528702da808917a15f2759425c
Praise Folorunso <folorunsopraise580@gmail.com>
2023-11-08 22:25:53 +0100

    Merchant table created

---
+++ database/migrations/2023_11_08_210210_create_merchant_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('merchant', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('business_name')->nullable(false);
+            $table->string('email')->unique()->nullable(false);
+            $table->integer('phone_number')->nullable(false);
+            $table->string('password')->nullable(false);
+            $table->string('referred_by')->nullable();
+            $table->boolean('t_and_c');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('merchant');
+    }
+};
