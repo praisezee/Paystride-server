dee36446efd91167b6951b94d5b09848ea2483f4
jumzeey <umatonjayrex01@gmail.com>
2023-11-12 08:58:49 +0100

    added resendOtp logic

--- app/Http/Controllers/MerchantController.php
+++ app/Http/Controllers/MerchantController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use App\Mail\OtpMail;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use Laravel\Sanctum\PersonalAccessToken;
 class MerchantController extends Controller
@@ -60,9 +61,19 @@ class MerchantController extends Controller
         return response(['message' => 'OTP sent to your email for verification'], 200);
     }
 
-    private function sendOtpEmail($email, $otp)
+    private function sendOtpEmail($email)
     {
-        Mail::to($email)->send(new OtpMail($otp));
+        $otp = rand(100000, 999999);
+        $merchant = Merchant::where('email', $email)->first();
+
+        if ($merchant) {
+            $merchant->update(['otp' => $otp]);
+
+            Mail::to($email)->send(new OtpMail($otp));
+        } else {
+            // Log if merchant is not found
+            info('Merchant Not Found for Email: ' . $email);
+        }
     }
 
     /**
@@ -70,7 +81,9 @@ class MerchantController extends Controller
      */
     public function verifyEmail(Request $request)
     {
+        info('Verification Request Data: ' . json_encode($request->all()));
         $request->validate([
+            'email' => 'required|email',
             'otp' => 'required|numeric',
         ]);
 
@@ -81,10 +94,10 @@ class MerchantController extends Controller
 
         if ($merchant) {
             // Update user status to verified
-            $merchant->update(['email_verified_at' => now()]);
-
-            // Optionally, you can clear the stored OTP
-            $merchant->update(['otp' => null]);
+            $merchant->update([
+                'email_verified_at' => now(),
+                'otp' => null, // Optionally, you can clear the stored OTP
+            ]);
 
             // Generate a token for the verified merchant
             $token = $merchant->createToken('merchant-token')->plainTextToken;
@@ -92,6 +105,51 @@ class MerchantController extends Controller
             return response(['message' => 'Email verified successfully', 'data' => $merchant, 'token' => $token], 200);
         } else {
             return response(['message' => 'Invalid OTP or email'], 400);
+        }
+    }
+     /**
+     * Resend OTP to the user's email.
+     */
+    public function resendOtp(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $merchant = Merchant::where('email', $request->email)
+            ->whereNull('email_verified_at')
+            ->first();
+
+        if ($merchant) {
+            // Check if there is a recent OTP resend attempt (within the last minute)
+            $recentResendAttempt = DB::table('otp_resend_attempts')
+                ->where('email', $request->email)
+                ->where('created_at', '>', now()->subMinute(5))
+                ->first();
+
+            if ($recentResendAttempt) {
+                // Resend attempt too soon, return an error
+                return response(['message' => 'Too many resend attempts. Try again later.'], 429);
+            }
+
+            // Generate a new OTP
+            $otp = rand(100000, 999999);
+
+            // Update the user's OTP
+            $merchant->update(['otp' => $otp]);
+
+            // Record the resend attempt
+            DB::table('otp_resend_attempts')->insert([
+                'email' => $request->email,
+                'created_at' => now(),
+            ]);
+
+            // Send OTP to user's email
+            $this->sendOtpEmail($request->email);
+
+            return response(['message' => 'OTP resent to your email for verification'], 200);
+        } else {
+            return response(['message' => 'Invalid email or email already verified'], 400);
         }
     }
     /**
