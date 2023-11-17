7b480afd1226583f3d2153ff20fac13650187b9c
Ubuntu <ubuntu@ip-172-31-85-57.ec2.internal>
2023-11-17 18:40:11 +0000

    infrastructure stagging

---
+++ .infrastructure/stagging/instance.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "paystride-backend"{
+  ami           = "ami-00874d747dde814fa"
+  instance_type = "t2.small"
+  key_name       = "paystride"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.paysrtide_sg-stagging.id]
+tags = {
+    Name = "paystride-backend-prod-stagging"
+  }
+}
+
---
+++ .infrastructure/stagging/sg.tf
@@ -0,0 +1,41 @@
+# Aws security group for paystride backend 
+resource "aws_security_group" "paysrtide_sg-stagging" {
+  name   = "paystride_security_group-stagging "
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
