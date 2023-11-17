3ee4a2c0c1237fb8b8118987d2ccafa1604792c9
Ubuntu <ubuntu@ip-172-31-85-57.ec2.internal>
2023-11-17 12:48:31 +0000

    infrastructure update

--- .infrastructure/prod/sg.tf
+++ .infrastructure/prod/sg.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "paysrtide_backend" {
 
 # Aws security group for paystride backend 
 resource "aws_security_group" "paysrtide_db" {
-  name   = "HTTP and SSH"
+  name   = "database_sg"
   vpc_id = aws_vpc.main.id
 
   ingress {
