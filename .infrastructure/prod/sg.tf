18dec43d6600f2a023d33930bcb59dfa1afbb123
Ubuntu <ubuntu@ip-172-31-85-57.ec2.internal>
2023-11-20 11:38:40 +0000

    infrastructure

--- .infrastructure/prod/sg.tf
+++ .infrastructure/prod/sg.tf
@@ -11,6 +11,13 @@ resource "aws_security_group" "paysrtide_backend" {
   }
 
   ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
