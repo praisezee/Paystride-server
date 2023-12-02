ab4a78e4d5a5a14bea142ac426b56bb2c0c4ce33
Praise Folorunso <folorunsopraise580@gmail.com>
2023-12-02 14:38:20 +0100

    Paymen point error fixed

--- app/Models/Payment_point.php
+++ app/Models/Payment_point.php
@@ -9,6 +9,13 @@ class Payment_point extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'merchant_id',
+        'staff_id',
+        'status'
+    ];
+
     public function merchant(){
         return $this->belongsTo(Merchant::class);
     }
