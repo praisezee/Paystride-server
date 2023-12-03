2e7398b4787158dee53a8c55b29c2abd01e9afc9
Ezeanyim henry <ezeanyimhenry@gmail.com>
2023-12-03 16:08:47 +0100

    Updated DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UserSeeder::class,
+            MerchantSeeder::class,
+            StaffSeeder::class,
+            PaymentPointSeeder::class,
+            VirtualAccountSeeder::class,
+            TransactionSeeder::class,
+        ]);
     }
 }
