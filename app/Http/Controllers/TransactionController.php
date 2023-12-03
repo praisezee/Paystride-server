27a4939f9b5fccd9ec5225924c2bdda2cb86b069
Ezeanyim henry <ezeanyimhenry@gmail.com>
2023-12-03 16:35:32 +0100

    fix: all transaction eloquent

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -5,60 +5,63 @@ namespace App\Http\Controllers;
 use App\Models\PaymentPoint;
 use App\Models\Staff;
 use App\Models\Transaction;
+use App\Models\VirtualAccount;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 
 class TransactionController extends Controller
 {
     public function index()
-{
-    // Retrieve all transactions with virtual account information
-    $transactions = Transaction::with('virtualAccount')->get();
+    {
+        // Retrieve all transactions with virtual account and payment point information
+        $transactions = Transaction::with(['virtualAccount', 'paymentPoint.staff'])->get();
 
-    // Retrieve unique payment point IDs from transactions
-    $paymentPointIds = $transactions->pluck('payment_point_id')->unique();
+        // Add staff_phone_number and virtual_account_number to each transaction
+        $transactions->transform(function ($transaction) {
+            $transaction->staff_phone_number = optional($transaction->paymentPoint->staff)->phone_number;
+            $transaction->virtual_account_number = optional($transaction->virtualAccount)->account_number;
+            return $transaction;
+        });
 
-    // Retrieve staff information for the unique payment point IDs
-    $staff = Staff::whereIn('id', $paymentPointIds)->get()->keyBy('id');
+        $response = [
+            'success' => true,
+            'statusCode' => 200,
+            'message' => $transactions->isNotEmpty() ? 'Successfully fetched all transactions' : 'No transactions found',
+            'data' => $transactions,
+        ];
 
-    // Add staff_phone_number to each transaction
-    $transactions->transform(function ($transaction) use ($staff) {
-        $transaction->staff_phone_number = $staff[$transaction->payment_point_id]->phone_number ?? null;
-        return $transaction;
-    });
-
-    $response = [
-        'success' => true,
-        'statusCode' => 200,
-        'message' => $transactions->isNotEmpty() ? 'Successfully fetched all transactions' : 'No transactions found',
-        'data' => $transactions,
-    ];
-
-    return response()->json($response);
-}
+        return response()->json($response);
+    }
 
 
-    public function create(){
+
+    public function create()
+    {
         // Show the form to create a new transaction
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         // Store a new transaction in the database
     }
 
-    public function show(Transaction $transaction){
+    public function show(Transaction $transaction)
+    {
         // Show details of a specific transaction
     }
 
-    public function edit(Transaction $transaction){
+    public function edit(Transaction $transaction)
+    {
         // Show the form to edit a transaction
     }
 
-    public function update(Request $request, Transaction $transaction){
+    public function update(Request $request, Transaction $transaction)
+    {
         // Update the function in the database
     }
 
-    public function destroy(Transaction $transaction){
+    public function destroy(Transaction $transaction)
+    {
         // Restrict deletion of transactions abort(403, 'Deletion of transactions is not allowed.');
     }
 }
