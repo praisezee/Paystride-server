3cabb00f580058db33ca3370ecfac355d7b3f80d
Praise Folorunso <folorunsopraise580@gmail.com>
2023-12-25 12:11:05 +0100

    staff login implemented

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\LoginResource;
+use App\Http\Resources\StaffResources;
 use App\Models\Merchant;
+use App\Models\Staff;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+
+use function PHPUnit\Framework\returnSelf;
 
 class LoginController extends Controller
 {
@@ -17,6 +21,10 @@ class LoginController extends Controller
             'password' => ['required', 'string']
         ]);
         if ($validator->fails()) return response(['message' => $validator->errors()->first()], 400);
+        $staff = Staff::where('email',$request->email)->first();
+        if($staff){
+            return $this->staffLogin($request->email,$request->password);
+        }
 
         $merchant = Merchant::where('email',$request->email)->first();
 
@@ -34,6 +42,24 @@ class LoginController extends Controller
 
     }
 
+
+    private function staffLogin ($email,$password) {
+        $staff = Staff::where('email',$email)->first();
+        $passwordMatch = Hash::check($password, $staff->password);
+
+        if ( !$passwordMatch) return response(['message'=>"Invalid Login credentials"],401);
+        $token = $staff->createToken("token")->plainTextToken;
+        $staffResource = new StaffResources($staff);
+
+        return response([
+            'message'=>'Logged in successfully',
+            'token'=>$token,
+            'data'=>$staffResource
+        ],200);
+
+
+
+    }
     /* public function logout(Request $request){
         $request->user()->currentAccessToken()->delete();
         return response(['message'=>"Logged out successfully"],200);
